2c266d1485f136c07b2fb3d19a8faaac6750d90e
Helder Pereira <phelder@gmail.com>
2021-08-30 19:49:00 +0200

    Exercise 2

--- code/lesson4/v1-we_are_back/src/App.vue
+++ code/lesson4/v1-we_are_back/src/App.vue
@@ -1,14 +1,36 @@
 <template>
   <SimpleComponent name="Helder" city="Porto" />
-  <SimpleComponent />
+  <SimpleComponent name="Bob" city="x" />
+  <SimpleComponent name="Joe" :warning="true" />
+
+  <ColorComponent :colors="['red', 'green']" />
+  <ColorComponent :colors="list1" />
+  <ColorComponent :colors="list2" />
 </template>
 
 <script>
 import SimpleComponent from "./components/SimpleComponent.vue";
+import ColorComponent from "./components/ColorComponent.vue";
+
+// EXERCISE:
+// 1 - Create a new component Color component
+// 2 - that component should receive an array of colors as a prop. Ex: ["red", "green", "#000"]
+// 2.1 - For each element in the array create a circle
+// 2.2 - The circle should have the respective color
+// 3 - The component should display in a row a circle for each color in the prop
+// 4 - Odd circles should have a black border, Even circles should have a grey border
+// 5 - Show the name of the color inside the circle
 
 export default {
   components: {
     SimpleComponent,
+    ColorComponent,
+  },
+  data: function () {
+    return {
+      list1: ["red", "green", "#00f"],
+      list2: ["yellow", "pink", "grey", "#ac345a"],
+    };
   },
 };
 </script>
--- code/lesson4/v1-we_are_back/src/components/SimpleComponent.vue
+++ code/lesson4/v1-we_are_back/src/components/SimpleComponent.vue
@@ -1,14 +1,26 @@
 <template>
-  <p>Hello I am {{ name }} and I am from {{ city }}</p>
-  <p>Hello I am {{ name }}</p>
+  <!-- <p v-if="city === 'Porto'">Hello I am {{ name }} and I am from {{ city }}</p>
+  <p v-else-if="city">Hello I am {{ name }} and I am from {{ city }} 😭</p>
+  <p v-else>Hello I am {{ name }}</p> -->
+
+  <!-- <p :class="{ warningParagraph: warning === true }"></p> -->
+  <p :class="{ warningParagraph: warning }">
+    Hello I am {{ name }}
+    <span v-if="city">and I am from {{ city }}</span>
+  </p>
 </template>
 
 <script>
 // EXERCISE
-// Use v-if to only show the second sentence when the city is present (not undefined)
+// 1 - Use v-if to only show the second sentence when the city is present (not undefined)
+// 2 - add a new Boolean prop `Warning` change the background to Yellow if `Warning` is true
 
 export default {
   props: {
+    warning: {
+      type: Boolean,
+      default: false,
+    },
     name: {
       default: "Default name!",
     },
@@ -18,6 +30,11 @@ export default {
 </script>
 
 <style scoped>
+.warningParagraph {
+  background-color: yellow;
+  color: red;
+}
+
 p {
   color: lime;
   background-color: green;
