82e223cbd224b47af4090f57b44e524a24b3546c
Helder Pereira <phelder@gmail.com>
2021-08-30 18:45:04 +0200

    Exercise 1

---
+++ code/lesson4/v1-we_are_back/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <SimpleComponent name="Helder" city="Porto" />
+  <SimpleComponent />
+</template>
+
+<script>
+import SimpleComponent from "./components/SimpleComponent.vue";
+
+export default {
+  components: {
+    SimpleComponent,
+  },
+};
+</script>
+
+<style>
+body {
+  background-color: #666;
+}
+</style>
---
+++ code/lesson4/v1-we_are_back/src/components/SimpleComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <p>Hello I am {{ name }} and I am from {{ city }}</p>
+  <p>Hello I am {{ name }}</p>
+</template>
+
+<script>
+// EXERCISE
+// Use v-if to only show the second sentence when the city is present (not undefined)
+
+export default {
+  props: {
+    name: {
+      default: "Default name!",
+    },
+    city: String,
+  },
+};
+</script>
+
+<style scoped>
+p {
+  color: lime;
+  background-color: green;
+
+  padding: 20px;
+  margin: 0 0 20px 0;
+}
+</style>
