f1caf5a93beb95e64675cadcecec706b023b9649
Helder Pereira <phelder@gmail.com>
2021-09-16 11:53:45 +0100

    MockData example

---
+++ examples/mockdata/mock-example/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col content-center">
+    <h1 class="text-4xl text-center my-8">Example List</h1>
+    <UserList />
+  </div>
+</template>
+
+<script>
+import UserList from "./components/UserList.vue";
+
+export default {
+  name: "App",
+  components: {
+    UserList,
+  },
+};
+</script>
+
+<style scoped></style>
+>
---
+++ examples/mockdata/mock-example/src/components/UserList.vue
@@ -0,0 +1,39 @@
+<template>
+  <table class="table-auto text-center">
+    <thead>
+      <tr>
+        <th px-4 py-2>Name</th>
+        <th px-4 py-2>Email</th>
+        <th px-4 py-2>Website</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(user, i) in apiResponseData" :key="i">
+        <td class="border px-4 py-2">{{ user.name }}</td>
+        <td class="border px-4 py-2">{{ user.email }}</td>
+        <td class="border px-4 py-2">{{ user.website }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      apiResponseData: undefined,
+    };
+  },
+  mounted: async function () {
+    const url = "http://localhost:8080/mockData.json";
+    // const url = "https://jsonplaceholder.typicode.com/users";
+
+    const response = await fetch(url);
+    const result = await response.json();
+
+    this.apiResponseData = result;
+  },
+};
+</script>
+
+<style></style>
